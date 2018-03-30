e214563e2368328980a94924b84c6c70167ed932
Marta Jopp <martapeterson@gmail.com>
2018-03-30 13:08:32 -0500

    server up and auth correct

--- server/auth.js
+++ server/auth.js
@@ -1,23 +1,28 @@
 //passport configuration file
-
-const GoogleStrategy = require('passport-googl-oauth').OAuth2Strategy;
+/* dotenv fetches credentials stored in .env file*/
+require('dotenv').config();
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 var CLIENT_ID = process.env.CLIENT_ID;
 var CLIENT_SECRET = process.env.CLIENT_SECRET;
-var CALL_BACK = process.env.CLIENT_SECRET
+var CALL_BACK = process.env.CALL_BACK;
 
 module.exports = (passport) => {
-    passport.serializeUser((user, done) =>{
+    passport.serializeUser((user, done) => {
         done(null, user);
     });
-
-    passport.deserializeUser((user, done) =>{
+    passport.deserializeUser((user, done) => {
         done(null, user);
-    })
-
-    passport.user(new GoogleStrategy({
-        clientId: CLIENT_ID,
+    });
+    passport.use(new GoogleStrategy({
+        clientID: CLIENT_ID,
         clientSecret: CLIENT_SECRET,
         callbackURL: 'http://localhost:5000/auth/google/callback'
-    }))
-} //end exports
+        },
+        (token, refreshToken, profile, done) => {
+            return done(null, {
+                profile: profile,
+                token: token
+            });
+        }));
+};
