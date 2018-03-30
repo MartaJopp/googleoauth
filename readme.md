27ac9a9f815b2e5fc572b2e7a9c9bcf8bc088be6
Marta Jopp <martapeterson@gmail.com>
2018-03-30 12:51:49 -0500

    auth

---
+++ readme.md
@@ -0,0 +1,1 @@
+Implementing Google Oauth Tutorial Walkthrough
--- server/auth.js
+++ server/auth.js
@@ -2,9 +2,22 @@
 
 const GoogleStrategy = require('passport-googl-oauth').OAuth2Strategy;
 
+var CLIENT_ID = process.env.CLIENT_ID;
+var CLIENT_SECRET = process.env.CLIENT_SECRET;
+var CALL_BACK = process.env.CLIENT_SECRET
+
 module.exports = (passport) => {
     passport.serializeUser((user, done) =>{
         done(null, user);
     });
 
+    passport.deserializeUser((user, done) =>{
+        done(null, user);
+    })
+
+    passport.user(new GoogleStrategy({
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        callbackURL: 'http://localhost:5000/auth/google/callback'
+    }))
 } //end exports
